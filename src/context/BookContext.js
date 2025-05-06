acff74bcb54b32b2a4c0af6a92b9fff3b0ae3ac0
Cristian <cp5221373@gmail.com>
2025-05-06 14:46:53 -0600

    1

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,7 +1,5 @@
 import { createContext, useContext } from "react"
 
 export const BookContext = createContext()
+export const useBooks = () => useContext(BookContext)
 
-export const useBooks = () => {
-  return useContext(BookContext)
-}
